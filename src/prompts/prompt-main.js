3b7d36998e54f4dbd87e8b2bb3abb63dc5486f0f
Rayaninha <salesrayane11@gmail.com>
2024-07-01 15:14:53 -0300

    feat: import prompt

---
+++ src/prompts/prompt-main.js
@@ -0,0 +1,9 @@
+const mainPrompt = [
+    {
+        name: "select",
+        description: "Choose the tool [1]-QRCODE or [2]-PASSWORD",
+        pattern: /^[1-2]+$/,
+        message: "Invalid number",
+        required: true
+    }
+]
