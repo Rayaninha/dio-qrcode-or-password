784a0d1b1f3547f7873be46363b712a2884305e9
Rayaninha <salesrayane11@gmail.com>
2024-07-01 15:32:07 -0300

    chore: architecture

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import prompt from "prompt"
 import mainPrompt from "./prompts/prompt-main.js"
+import createQRCode from "./services/qrCode/create.js"
 
 (async function main() { })(
     prompt.get(mainPrompt, async (err, choose) => {
         if (choose.select == 1) {
             console.log("QRCODE")
+            await createQRCode()
         }
 
         if (choose.select == 2) {
