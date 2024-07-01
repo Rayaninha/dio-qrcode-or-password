1a66be8b0315d7edf54e4370c325565e05fcd56d
Rayaninha <salesrayane11@gmail.com>
2024-07-01 16:10:07 -0300

    feat: generate qrcode

--- src/prompts/prompt-qrcode.js
+++ src/prompts/prompt-qrcode.js
@@ -1,5 +1,16 @@
 import chalk from "chalk";
 
-const promptQRCode = [];
+const promptQRCode = [
+    {
+        name: "link",
+        description: chalk.yellow("Insert the link to generate the QRCODE")
+    },
+    {
+        name: "type",
+        description: chalk.yellow("Choose between types [1]-IMAGE or [2]-TERMINAL"),
+        pattern: /^[1-2]+$/,
+        message: chalk.red.italic("Invalid number")
+    }
+];
 
 export default promptQRCode;
